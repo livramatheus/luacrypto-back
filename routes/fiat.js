a248f985ba845e35c0d34707b3001c3706ca0527
livramateus <livra.matheus@gmail.com>
2021-10-15 21:55:52 -0300

    Adicionado cache no endpoint de fiat

--- routes/fiat.js
+++ routes/fiat.js
@@ -6,19 +6,34 @@ const redisClient = require('../config/redis');
 /**
  * Retorna dados gerais e a última cotação da fiat passada como parâmetro
  */
-router.get('/fiat/:chave', (req, res) => {
+router.get('/fiat/:chave', async (req, res) => {
     let chave = req.params.chave.toUpperCase();
 
-    controllerFiat.getDadosGeraisRecentes(chave).then((resultado) => {
-        res.send(resultado);
+    const CACHE_SECONDS = 1800; // 30 minutos / 1800 segundos
+    const CACHE_NAME    = `fiat_chave_${chave}`;
+
+    const respostaCache = await redisClient.getCache(CACHE_NAME).then((dadosCache) => {
+        return dadosCache;
     });
+
+    if (respostaCache) {
+        res.send(respostaCache);
+    } else {
+        controllerFiat.getDadosGeraisRecentes(chave).then((resultadoBanco) => {
+            redisClient.setCache(CACHE_NAME, JSON.stringify(resultadoBanco), CACHE_SECONDS).then((resultadoRedis) => {
+                if (resultadoRedis) {
+                    res.send(resultadoBanco);
+                }
+            })
+        });
+    }
 });
 
 /**
  * Devolve todos os registros completos
  */
 router.get('/todasfiatscompletas', async (req, res) => {
-    const CACHE_SECONDS = 3600; // 1 hora minutos / 3600 segundos
+    const CACHE_SECONDS = 3600; // 1 hora / 3600 segundos
     const CACHE_NAME    = `todas_fiats_completas`;
 
     const respostaCache = await redisClient.getCache(CACHE_NAME).then((dadosCache) => {
